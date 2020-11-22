55f200c5f8b3553d324a0f81a5348de766937b96
11philip22 <philipwoldhek@gmail.com>
2020-11-22 19:00:51 +0100

    logo

--- MEGAabuse.py
+++ MEGAabuse.py
@@ -21,6 +21,25 @@ from pathlib import Path
 from megaabuse import MegaAbuse, get_logger
 from megaabuse.macqueue import Queue
 
+print("""
+888b     d888 8888888888 .d8888b.         d8888 
+8888b   d8888 888       d88P  Y88b       d88888 
+88888b.d88888 888       888    888      d88P888 
+888Y88888P888 8888888   888            d88P 888 
+888 Y888P 888 888       888  88888    d88P  888 
+888  Y8P  888 888       888    888   d88P   888 
+888   "   888 888       Y88b  d88P  d8888888888 
+888       888 8888888888 "Y8888P88 d88P     888 
+         888                                    
+         888                                    
+         888                                    
+ 8888b.  88888b.  888  888 .d8888b   .d88b.     
+    "88b 888 "88b 888  888 88K      d8P  Y8b    
+.d888888 888  888 888  888 "Y8888b. 88888888    
+888  888 888 d88P Y88b 888      X88 Y8b.        
+"Y888888 88888P"   "Y88888  88888P'  "Y8888     
+""")
+
 # Parse arguments
 PARSER = argparse.ArgumentParser(description="MEGAabuse")
 
