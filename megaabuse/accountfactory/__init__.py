73c3c0b7cb178c9f08f7894877231f4cc9933c31
11philip22 <philipwoldhek@gmail.com>
2021-04-23 17:11:00 +0200

    sdk

--- megaabuse/accountfactory/__init__.py
+++ megaabuse/accountfactory/__init__.py
@@ -14,7 +14,7 @@ from datetime import datetime
 from pathlib import Path
 from random import choice
 
-import mariadb
+# import mariadb
 # from bs4 import BeautifulSoup
 from names import get_first_name
 
@@ -74,173 +74,173 @@ class AccountFactory:
         return None
 
 
-class IGenMail(AccountFactory, DovecotSSHA512Hasher):
-    """"Creates mega.nz accounts using iRedMail
-
-    This is designed to work with mariadb.
-
-    """
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        super(DovecotSSHA512Hasher, self).__init__(prefix='{SSHA512}')
-
-        self.conn = None
-
-    def connect_to_db(self, host, user, password, port=3306, db="vmail"):
-        """" Connects to DB """
-
-        try:
-            self.conn = mariadb.connect(
-                user=user,
-                password=password,
-                host=host,
-                port=port,
-                database=db
-            )
-            self.conn.autocommit = False
-            return True
-        except Exception as e:
-            self.logger.error(e)
-            return False
-
-    def create_mail_user(self, email, password):
-        """" Creates a new mail user
-
-        Cleanup ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓ DELETE ALL USERS EXCEPT POSTMASTER
-        DELETE FROM mailbox WHERE NOT username = 'postmaster@domain.com';
-        DELETE FROM forwardings WHERE NOT address =  'postmaster@domain.com';
-
-        Delete every user except postmaster ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
-        DELETE FROM mailbox WHERE NOT username = 'postmaster@domain.com';
-        DELETE FROM forwardings WHERE NOT address =  'postmaster@domain.com';
-
-        """
-
-        if self.conn is None:
-            self.logger.error("Can't create user! Not connected to db")
-            return False
-
-        storage_base_dir = "/var/vmail/vmail1"
-        storage_base = str(Path(storage_base_dir).parent)
-        storage_node = str(Path(storage_base_dir).name)
-        crypt_password = self.encode(password)  # Encode password to SSHA512 in dovecot format
-        s_uname = email.split("@")
-        user_name = s_uname[0]
-        domain = s_uname[1]
-
-        now = datetime.now()  # Get current timestamp
-        dt_str = now.strftime("%Y.%m.%d.%H.%M.%S")
-
-        mail_dir = f"{domain}/{email[0]}/{email[1]}/{email[2]}/{email}-{dt_str}"
-
-        try:
-            cursor = self.conn.cursor()
-            cursor.execute("""INSERT INTO mailbox (username, password, name, 
-                                                   storagebasedirectory,storagenode, maildir, 
-                                                   quota, domain, active, passwordlastchange, created) 
-                                           VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, NOW(), NOW());""",
-                           (email, crypt_password, user_name, storage_base, storage_node, mail_dir, "1024", domain, 1))
-            cursor.execute("""INSERT INTO forwardings (address, forwarding, domain, dest_domain, is_forwarding)
-                                               VALUES (?, ?, ?, ?, 1);""",
-                           (email, email, domain, domain))
-            self.conn.commit()
-            return True
-        except mariadb.Error as e:
-            self.logger.error(e)
-            return False
-
-    def delete_mail_user(self, email):
-        """" Delete mail user """
-        try:
-            cursor = self.conn.cursor()
-            cursor.execute("DELETE FROM mailbox WHERE username=?;", (email,))
-            cursor.execute("DELETE FROM forwardings WHERE address=?;", (email,))
-            self.conn.commit()
-            return True
-        except mariadb.Error as e:
-            self.logger.error(e)
-            return False
-
-    def cleanup_users(self, domain):
-        """" Deletes all users except for postmaster """
-        cursor = self.conn.cursor()
-        username = f"postmaster@{domain}"
-        cursor.execute(
-            f"""DELETE FROM mailbox WHERE NOT username=?;
-                DELETE FROM forwardings WHERE NOT address=?;""",
-            (username, username)
-        )
-        self.conn.commit()
-
-    def create_mega_account(self, domain, imap_address, fixed_pass, proxy):
-        """" Create a mega.nz account using iGenMail
-
-        Designed to work with maria DB. Adjustments need to be made to make it work with postgresql
-        but the sql statements are the same.
-
-        """
-        self.logger.info("Registering accounts")
-
-        email = f"{self.random_mail()}@{domain}"
-        name = get_first_name()
-
-        if fixed_pass:
-            email_password = fixed_pass
-        else:
-            email_password = self.random_text(21)
-
-        self.create_mail_user(email, email_password)  # Create an mail user
-
-        # Register new mega account
-        cmd = f"{self.megareg_dir} -n {name} -e {email} -p {email_password} --register --scripted"
-        if proxy:
-            cmd += f" --proxy={proxy}"
-        self.logger.debug(cmd)
-
-        confirm_text = subprocess.check_output(cmd, shell=True).decode('UTF-8')
-        confirm_text = confirm_text[confirm_text.find("-"):]
-        self.logger.info("Done registering")
-
-        # Wait for mail
-        wait_time = 0
-        while True:
-            if wait_time > 120:
-                self.logger.error("Waiting for mail time exceeded 2 minutes. Aborting")
-                raise WaitForMailTimoutException(wait_time)
-
-            imap = imaplib.IMAP4_SSL(imap_address)
-            if not imap.login(email, email_password)[0] == "OK":
-                self.logger.error("Could not login to the mail server")
-                return False, False
-
-            imap.select("inbox")
-
-            welcome_mail = imap.search(None, "FROM", '"welcome@mega.nz"')[1]
-            if welcome_mail == [b'']:  # If mailbox contains no mail
-                imap.close()
-                imap.logout()
-                wait_time += 2
-                time.sleep(2)
-                continue
-            else:
-                self.logger.info("Got mail")
-                break
-
-        data = imap.search(None, "FROM", '"welcome@mega.nz"')[1]
-        typ, data = imap.fetch(data[0], '(RFC822)')
-        raw_email = data[0][1]
-        raw_email_string = raw_email.decode('utf-8')
-        confirm_url = self.extract_url(raw_email_string)  # Extract confirm url from mail
-
-        imap.close()
-        imap.logout()
-
-        # Confirm new account
-        confirm_command = f"{self.megareg_dir} {confirm_text.replace('@LINK@', confirm_url)}"
-        subprocess.check_output(confirm_command, shell=True)
-
-        return email, email_password
+# class IGenMail(AccountFactory, DovecotSSHA512Hasher):
+#     """"Creates mega.nz accounts using iRedMail
+#
+#     This is designed to work with mariadb.
+#
+#     """
+#
+#     def __init__(self, **kwargs):
+#         super().__init__(**kwargs)
+#         super(DovecotSSHA512Hasher, self).__init__(prefix='{SSHA512}')
+#
+#         self.conn = None
+#
+#     def connect_to_db(self, host, user, password, port=3306, db="vmail"):
+#         """" Connects to DB """
+#
+#         try:
+#             self.conn = mariadb.connect(
+#                 user=user,
+#                 password=password,
+#                 host=host,
+#                 port=port,
+#                 database=db
+#             )
+#             self.conn.autocommit = False
+#             return True
+#         except Exception as e:
+#             self.logger.error(e)
+#             return False
+#
+#     def create_mail_user(self, email, password):
+#         """" Creates a new mail user
+#
+#         Cleanup ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓ DELETE ALL USERS EXCEPT POSTMASTER
+#         DELETE FROM mailbox WHERE NOT username = 'postmaster@domain.com';
+#         DELETE FROM forwardings WHERE NOT address =  'postmaster@domain.com';
+#
+#         Delete every user except postmaster ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+#         DELETE FROM mailbox WHERE NOT username = 'postmaster@domain.com';
+#         DELETE FROM forwardings WHERE NOT address =  'postmaster@domain.com';
+#
+#         """
+#
+#         if self.conn is None:
+#             self.logger.error("Can't create user! Not connected to db")
+#             return False
+#
+#         storage_base_dir = "/var/vmail/vmail1"
+#         storage_base = str(Path(storage_base_dir).parent)
+#         storage_node = str(Path(storage_base_dir).name)
+#         crypt_password = self.encode(password)  # Encode password to SSHA512 in dovecot format
+#         s_uname = email.split("@")
+#         user_name = s_uname[0]
+#         domain = s_uname[1]
+#
+#         now = datetime.now()  # Get current timestamp
+#         dt_str = now.strftime("%Y.%m.%d.%H.%M.%S")
+#
+#         mail_dir = f"{domain}/{email[0]}/{email[1]}/{email[2]}/{email}-{dt_str}"
+#
+#         try:
+#             cursor = self.conn.cursor()
+#             cursor.execute("""INSERT INTO mailbox (username, password, name,
+#                                                    storagebasedirectory,storagenode, maildir,
+#                                                    quota, domain, active, passwordlastchange, created)
+#                                            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, NOW(), NOW());""",
+#                            (email, crypt_password, user_name, storage_base, storage_node, mail_dir, "1024", domain, 1))
+#             cursor.execute("""INSERT INTO forwardings (address, forwarding, domain, dest_domain, is_forwarding)
+#                                                VALUES (?, ?, ?, ?, 1);""",
+#                            (email, email, domain, domain))
+#             self.conn.commit()
+#             return True
+#         except mariadb.Error as e:
+#             self.logger.error(e)
+#             return False
+#
+#     def delete_mail_user(self, email):
+#         """" Delete mail user """
+#         try:
+#             cursor = self.conn.cursor()
+#             cursor.execute("DELETE FROM mailbox WHERE username=?;", (email,))
+#             cursor.execute("DELETE FROM forwardings WHERE address=?;", (email,))
+#             self.conn.commit()
+#             return True
+#         except mariadb.Error as e:
+#             self.logger.error(e)
+#             return False
+#
+#     def cleanup_users(self, domain):
+#         """" Deletes all users except for postmaster """
+#         cursor = self.conn.cursor()
+#         username = f"postmaster@{domain}"
+#         cursor.execute(
+#             f"""DELETE FROM mailbox WHERE NOT username=?;
+#                 DELETE FROM forwardings WHERE NOT address=?;""",
+#             (username, username)
+#         )
+#         self.conn.commit()
+#
+#     def create_mega_account(self, domain, imap_address, fixed_pass, proxy):
+#         """" Create a mega.nz account using iGenMail
+#
+#         Designed to work with maria DB. Adjustments need to be made to make it work with postgresql
+#         but the sql statements are the same.
+#
+#         """
+#         self.logger.info("Registering accounts")
+#
+#         email = f"{self.random_mail()}@{domain}"
+#         name = get_first_name()
+#
+#         if fixed_pass:
+#             email_password = fixed_pass
+#         else:
+#             email_password = self.random_text(21)
+#
+#         self.create_mail_user(email, email_password)  # Create an mail user
+#
+#         # Register new mega account
+#         cmd = f"{self.megareg_dir} -n {name} -e {email} -p {email_password} --register --scripted"
+#         if proxy:
+#             cmd += f" --proxy={proxy}"
+#         self.logger.debug(cmd)
+#
+#         confirm_text = subprocess.check_output(cmd, shell=True).decode('UTF-8')
+#         confirm_text = confirm_text[confirm_text.find("-"):]
+#         self.logger.info("Done registering")
+#
+#         # Wait for mail
+#         wait_time = 0
+#         while True:
+#             if wait_time > 120:
+#                 self.logger.error("Waiting for mail time exceeded 2 minutes. Aborting")
+#                 raise WaitForMailTimoutException(wait_time)
+#
+#             imap = imaplib.IMAP4_SSL(imap_address)
+#             if not imap.login(email, email_password)[0] == "OK":
+#                 self.logger.error("Could not login to the mail server")
+#                 return False, False
+#
+#             imap.select("inbox")
+#
+#             welcome_mail = imap.search(None, "FROM", '"welcome@mega.nz"')[1]
+#             if welcome_mail == [b'']:  # If mailbox contains no mail
+#                 imap.close()
+#                 imap.logout()
+#                 wait_time += 2
+#                 time.sleep(2)
+#                 continue
+#             else:
+#                 self.logger.info("Got mail")
+#                 break
+#
+#         data = imap.search(None, "FROM", '"welcome@mega.nz"')[1]
+#         typ, data = imap.fetch(data[0], '(RFC822)')
+#         raw_email = data[0][1]
+#         raw_email_string = raw_email.decode('utf-8')
+#         confirm_url = self.extract_url(raw_email_string)  # Extract confirm url from mail
+#
+#         imap.close()
+#         imap.logout()
+#
+#         # Confirm new account
+#         confirm_command = f"{self.megareg_dir} {confirm_text.replace('@LINK@', confirm_url)}"
+#         subprocess.check_output(confirm_command, shell=True)
+#
+#         return email, email_password
 
 
 class GuerrillaGen(AccountFactory):
